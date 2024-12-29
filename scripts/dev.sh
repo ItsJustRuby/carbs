38895f5fc966b46ccd45de29f1641e3d840c4648
Ruby <3r4o02hucyw7o9fekpx8gla2@pollination.email>
2024-12-29 07:31:42 +0100

    Fix tests

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -ev
 
-env COMPOSE_FILE=docker-compose.dev.yml \
+env COMPOSE_PATH_SEPARATOR=: \
+    COMPOSE_FILE=docker-compose.yml:docker-compose.dev.yml \
     docker compose up -d --build
--- scripts/tests.sh
+++ scripts/tests.sh
@@ -2,5 +2,5 @@
 set -ev
 
 env COMPOSE_PATH_SEPARATOR=: \
-    COMPOSE_FILE=docker-compose.dev.yml:docker-compose.tests.yml \
+    COMPOSE_FILE=docker-compose.yml:docker-compose.tests.yml \
     docker compose up -d --build
